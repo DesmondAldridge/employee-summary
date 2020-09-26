9d9a8fa6903349cf714571e31dfad88200f9e3a9
Desmond <desmondaldridge@gmail.com>
2020-09-25 18:37:50 -0700

    More set up, and beginning to write the prompts

--- Develop/app.js
+++ Develop/app.js
@@ -10,6 +10,91 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const projectTeamArray[];
+
+function managerPrompt() {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of your project's manager?"
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your project manager's ID?"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your project manager's email?",
+      },
+    ]).then(function(answers) {
+      let manager = new Manager(answers.name, answers.id, answers.email)
+      team.push(manager);
+  
+      addEmployee()
+    })
+  };
+
+function addEmployee() {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Please input a brief description of your project:"
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "Please input instructions on how to install your program:"
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "Please input instructions on how to use your program:"
+      },
+      {
+        type: "input",
+        name: "contribution",
+        message: "Please list all contributors and contributions to your program:"
+      },
+      {
+        type: "input",
+        name: "test",
+        message: "Please input instructions on how to test your program:"
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "Please choose a license for your project:",
+        choices: ["Academic Free License v3.0", "Apache 2.0", "Creative Commons", "wtfpl", "GPL", "MIT", "NONE", "OTHER"]
+      },
+      {
+        type: "input",
+        name: "license",
+        when: (answers) => answers.license === "OTHER",
+        message: "Please input the name of the license:"
+      },
+      {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+      },
+    ]);
+  }
+
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
